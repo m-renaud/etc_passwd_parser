b953044be5294b3e573019d45009d0d051d37cd8
Matt Renaud <mrenaud92@gmail.com>
2011-08-23 23:52:45 -0400

    Reworked parser with Paul's help

--- main.cxx
+++ main.cxx
@@ -1,9 +1,39 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <iomanip>
 
 #include "types.hxx"
 #include "parser.hxx"
 
+#define USE_INPUT_STREAM_ITER
+
+#ifdef USE_INPUT_STREAM_ITER
+#include <boost/spirit/include/support_multi_pass.hpp>
+#include <boost/spirit/include/support_istream_iterator.hpp>
+#include <boost/spirit/include/classic_position_iterator.hpp>
+
+typedef
+  std::istreambuf_iterator<char>
+  base_iter_type
+;
+
+typedef
+  boost::spirit::multi_pass<base_iter_type>
+  mp_iter_type
+;
+
+typedef
+  boost::spirit::classic::position_iterator2<mp_iter_type>
+  parse_iter_type
+;
+#endif
+
+void displayInfo(mrr::etc_pass_info const& epi);
+void displayInfoAsXML(mrr::etc_pass_info const& epi);
+
+
+//===========================================================================
 int main()
 {
   using namespace mrr;
@@ -11,15 +41,100 @@ int main()
   std::string input;
   std::string temp;
 
+#ifndef USE_INPUT_STREAM_ITER
   while(std::getline(std::cin, temp))
     input += temp + "\n";
+#endif
 
-  etc_pass_info info;
+#ifdef USE_INPUT_STREAM_ITER
+  std::cin.setf(std::ios_base::skipws);
+
+  // Create multipass iterators so the transient stream will be
+  // buffered...
+  mp_iter_type mp_beg = boost::spirit::make_default_multi_pass(
+    base_iter_type(std::cin)
+  );
+  mp_iter_type mp_end = boost::spirit::make_default_multi_pass(
+    base_iter_type()
+  );
+
+  // Create iterators that tracks the line and column position and
+  // also buffers the last line read in...
+  parse_iter_type begin(mp_beg, mp_end);
+  begin.set_tab_chars(8);
+
+  parse_iter_type end;
+#endif
+
+  //etc_pass_info info;
+  START_STATE_RULE_RETTYPE info;
+#ifdef USE_INPUT_STREAM_ITER
+  bool result = parse_etc_pass(begin, end, info);
+
+  if (result)
+  {
+//    std::cout << "Parsed fine." << std::endl;
+    displayInfoAsXML(info);
+  }
+  else
+  {
+    boost::spirit::classic::file_position_base<std::string> const& pos
+    = begin.get_position();
+
+    std::ostringstream buf;
+    buf
+    << "Parse error @ line "<< pos.line << " column " << pos.column
+    << std::endl
+    << '\'' << begin.get_currentline() << '\''
+    ;
+
+    if (!begin.get_currentline().empty())
+      buf << std::endl << std::setw(pos.column) << "" << "^here";
+
+    std::cout << buf.str() << std::endl;
+  }
+#else
   bool result = parse_etc_pass(input.begin(), input.end(), info);
 
-  if(result)
+  if (result)
     std::cout << "Parsed fine." << std::endl;
   else
     std::cout << "No parse." << std::endl;
+#endif
+
+
+}
+
+//===========================================================================
+void displayInfo(mrr::etc_pass_info const& epi)
+{
+  for(mrr::etc_pass_record const& epr : epi.records)
+  {
+    std::cout << "Username: " << epr.username << std::endl;
+    std::cout << "Password: " << epr.password << std::endl;
+    std::cout << "UID     : " << epr.uid      << std::endl;
+    std::cout << "GID     : " << epr.gid      << std::endl;
+    std::cout << "Uid Info: " << epr.uid_info << std::endl;
+    std::cout << "Home Dir: " << epr.home_dir << std::endl;
+    std::cout << "Shell   : " << epr.shell    << std::endl;
+  }
+}
+
+//===========================================================================
+void displayInfoAsXML(mrr::etc_pass_info const& epi)
+{
+  std::cout << "<etc_pass>" << std::endl;
+  for(mrr::etc_pass_record const& epr : epi.records)
+  {
+    std::cout << "  <record>" << std::endl;
+    std::cout << "    <username>" << epr.username << "</username>" << std::endl;
+    std::cout << "    <uid>"      << epr.uid     << "</uid>"       << std::endl;
+    std::cout << "    <gid>"      << epr.gid      << "</gid>"       << std::endl;
+    std::cout << "    <uid_info>" << epr.uid_info << "</uid_info>"  << std::endl;
+    std::cout << "    <home_dir>" << epr.home_dir << "</home_dir>"  << std::endl;
+    std::cout << "    <shell>"    << epr.shell    << "</shell>"     << std::endl;
+    std::cout << "  </record>" << std::endl;
+  }
+  std::cout << "</etc_pass>" << std::endl;
 
 }
--- parser.hxx
+++ parser.hxx
@@ -1,6 +1,24 @@
 #ifndef __MRR_ETC_PASSWD_PARSE_HXX__
 #define __MRR_ETC_PASSWD_PARSE_HXX__
 
+// TO FULLY DEBUG COMPILE WITH:
+//   g++ -DBOOST_SPIRIT_DEBUG main.cxx
+//   g++ -std=c++0x -DBOOST_SPIRIT_DEBUG main.cxx
+
+// Matt notice how I debugged things...
+//  I left the older macro definitions in as comments so you can see.
+
+//#define START_STATE_RULE_NAME         string_field
+//#define START_STATE_RULE_RETTYPE      std::string
+
+//#define START_STATE_RULE_NAME         record
+//#define START_STATE_RULE_RETTYPE      ::mrr::etc_pass_record
+
+#define START_STATE_RULE_NAME         start_state
+#define START_STATE_RULE_RETTYPE      ::mrr::etc_pass_info
+
+
+
 #define FUSION_MAX_VECTOR_SIZE 20
 
 #include "types.hxx"
@@ -8,6 +26,10 @@
 #include <boost/config/warning_disable.hpp>
 #include <boost/spirit/include/qi.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
+
+#include <boost/spirit/include/phoenix.hpp>
+
+#include <boost/spirit/repository/include/qi_flush_multi_pass.hpp>
 
 #include <vector>
 #include <string>
@@ -28,7 +50,6 @@ BOOST_FUSION_ADAPT_STRUCT(
   (std::vector<mrr::etc_pass_record>, records)
 )
 
-
 namespace mrr
 {
 
@@ -36,55 +57,85 @@ namespace mrr
 //  namespace ascii = boost::spirit::ascii;
 
   template <typename Iter>
-  struct parser : qi::grammar<Iter, etc_pass_info(), qi::eol_parser>
+  struct parser : qi::grammar<Iter, START_STATE_RULE_RETTYPE() >
   {
-    parser() : parser::base_type(start)
+    parser() : parser::base_type(START_STATE_RULE_NAME)
     {
       using qi::uint_;
       using qi::char_;
+      using qi::lit;
+      using qi::eoi;
+      using qi::omit;
+      using boost::spirit::repository::flush_multi_pass;
 //      using qi::lexeme;
 //      using qi::lit;
 //      using qi::eol;
 //      using ascii::alnum;
 
-      string_field %= +(char_ - ':');
-
-      record %= string_field
-      >> ':' >> string_field
-      >> ':' >> uint_
-      >> ':' >> uint_
-      >> ':' >> string_field
-      >> ':' >> string_field
-      >> ':' >> string_field
+      string_field %=
+        +(char_ -(lit(':') | lit('\n') | lit("\r\n")))
       ;
 
-      start %= +record;
+      record %=
+        string_field
+        >> ':' >> string_field
+        >> ':' >> uint_
+        >> ':' >> uint_
+        >> ':' >> string_field
+        >> ':' >> string_field
+        >> ':' >> string_field
+      ;
 
+      end_of_record %=
+        (
+          lit('\n')
+          | lit("\r\n")
+          | eoi
+        )
+        >> flush_multi_pass
+      ;
+
+      records %=
+        record % end_of_record
+      ;
+
+      start_state = records;
+
+#if 0
+#define SRN(s)    s.name(#s); debug(s);
+      SRN(string_field)
+      SRN(record)
+      SRN(end_of_record)
+      SRN(records)
+      SRN(start_state)
+#undef SRN
+#endif
     }
 
-    qi::rule<Iter, std::string()> string_field;
+    qi::rule<Iter, std::string()> string_field, end_of_record;
     qi::rule<Iter, etc_pass_record()> record;
-    qi::rule<Iter, etc_pass_info(), qi::eol_parser> start;
+    qi::rule<Iter, std::vector<etc_pass_record>()> records;
+    qi::rule<Iter, etc_pass_info()> start_state;
   };
 
   template <typename Iter>
   bool parse_etc_pass(
-    Iter first,
-    Iter last,
-    etc_pass_info& info
+    Iter& first,
+    Iter& last,
+    //etc_pass_info& info
+    START_STATE_RULE_RETTYPE& info
   )
   {
     parser<Iter> p;
 
-    bool result = qi::phrase_parse(
+    bool result = qi::parse(
       first,
       last,
       p,
-      qi::eol,
       info
     );
 
-    return (result && first == last);
+    return result;
   }
 
 } // namespace mrr
